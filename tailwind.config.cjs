f2b4b8a2be61d90578cab8c345905eede990ed52
May <mayra.denot@gmail.com>
2023-10-03 21:45:36 -0300

    Add Index Page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,9 +9,16 @@ module.exports = {
 			backgroundImage: {
 				"content": "linear-gradient(180deg, rgba(45,47,59,1) 0%, rgba(31,32,43,1) 81%)",
 				"header": "linear-gradient(180deg, rgba(0,0,0,1) 11%, rgba(31,32,43,0) 100%)",
+				"index": "url(//cnbl-cdn.bamgrid.com/assets/4f6c5f0c255fc3dc6f615e8a15efd071e34cd862f5ee7bd5c9f26e26f29cd639/original)",
+			},
+			backgroundColor: {
+				"btn-login": "rgba(0, 0, 0, .6)",
 			},
 			boxShadow: {
 				"cards": "rgba(0, 0, 0, 0.7) 0px 15px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;",
+			},
+			backgroundPosition: {
+				"p-index": "50% bottom",
 			},
 		},
 	},
