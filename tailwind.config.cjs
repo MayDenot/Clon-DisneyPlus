9785907a181b3f9a92bed0c8c8975e91c1589b80
May <mayra.denot@gmail.com>
2023-10-04 19:20:21 -0300

    Add Search Page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,9 +7,11 @@ module.exports = {
 				sans: ["Poppins", "sans-serif"],
 			},
 			backgroundImage: {
-				"content": "linear-gradient(180deg, rgba(45,47,59,1) 0%, rgba(31,32,43,1) 81%)",
-				"header": "linear-gradient(180deg, rgba(0,0,0,1) 11%, rgba(31,32,43,0) 100%)",
+				"content": "linear-gradient(180deg, rgba(45, 47, 59, 1) 0%, rgba(31, 32, 43, 1) 81%)",
+				"header": "linear-gradient(180deg, rgba(0, 0, 0, 1) 11%, rgba(31, 32, 43, 0) 100%)",
 				"index": "url(//cnbl-cdn.bamgrid.com/assets/4f6c5f0c255fc3dc6f615e8a15efd071e34cd862f5ee7bd5c9f26e26f29cd639/original)",
+				"search": "linear-gradient(180deg, rgba(0, 0, 0, 0.4) 0%, rgba(31, 32, 43, 0) 50%)",
+				"focus-s": "linear-gradient(180deg, rgba(0,0,0,0.3085609243697479) 0%, rgba(35,35,35,0) 33%, rgba(232,232,232,0.1) 97%)",
 			},
 			backgroundColor: {
 				"btn-login": "rgba(0, 0, 0, .6)",
